08acbc3eda37d79f0348d94ba4069b72dd2b2b63
Dovonun <31408384+Dovonun@users.noreply.github.com>
2025-05-08 18:03:25 +0900

    better cli usage

--- src/main.zig
+++ src/main.zig
@@ -5,23 +5,25 @@ pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const args = try std.process.argsAlloc(allocator);
 
-    if (args.len != 3) {
+    if (args.len < 2) {
         std.debug.print("Usage: {s} encode|decode <data>\n", .{args[0]});
         return;
     }
 
-    const mode = args[1];
-    const data = args[2];
-
-    if (std.mem.eql(u8, mode, "encode")) {
-        const result = b.encode(allocator, data);
+    if (args.len == 2) {
+        const result = b.encode(allocator, args[1]);
         defer allocator.free(result);
         try std.io.getStdOut().writer().print("{s}\n", .{result});
-    } else if (std.mem.eql(u8, mode, "decode")) {
-        const result = try b.decode(allocator, data);
-        defer allocator.free(result);
-        try std.io.getStdOut().writer().print("{s}\n", .{result});
-    } else {
-        std.debug.print("Unknown mode: {s}\n", .{mode});
+        return;
     }
+
+    if (std.mem.eql(u8, args[1], "-d")) {
+        const result = try b.decode(allocator, args[2]);
+        defer allocator.free(result);
+        try std.io.getStdOut().writer().print("{s}\n", .{result});
+        return;
+    }
+
+    std.debug.print("Usage: {s} [-d] <data>\n", .{args[0]});
+    return;
 }
